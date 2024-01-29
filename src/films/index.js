2c1f7b3301b9f7a736cdc38acd08d5737f8990e0
Dragonbestgg1 <dragonbestgg1@gmail.com>
2024-01-29 21:13:56 +0200

    .

---
+++ src/films/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../styles/films.module.css";
+
+function Films(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Films;
---
+++ src/user_profile/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "../styles/user.module.css";
+
+function Profile(){
+    return(
+        <div className={`${style.main}`}>
+
+        </div>
+    )
+}
+export default Profile;
