aba23e6dcad6d0c4cc645be0f788e1d5060536a2
Dragonbestgg1 <dragonbestgg1@gmail.com>
2024-01-30 14:05:33 +0200

    .

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,19 @@ import React from "react";
 import style from "../styles/header.module.css";
 import ReactModal from "react-modal";
 import Login from "./login";
-import { BiLogIn } from "react-icons/bi";
+import { BsPersonCircle } from "react-icons/bs";
 
 function Header(){
     return(
         <div className={`${style.header}`}>
             <div className={`${style.align}`}>
-                <a className={`${style.res}`} href="/">Home</a>
-                <a className={`${style.res}`} href="/films">Films</a>
-                <a className={`${style.res}`} href="/activities">Activities</a>
-                <a className={`${style.res}`} href="user_profile">Profile</a>
-                <button onClick className={`${style.log}`}><BiLogIn /></button> {/*<BiLogOut /> */}
+                <div className={`${style.routes}`}>
+                    <a className={`${style.res}`} href="/">Home</a>
+                    <a className={`${style.res}`} href="/films">Films</a>
+                    <a className={`${style.res}`} href="/activities">Activities</a>
+                    <a className={`${style.res}`} href="/user_profile">Profile</a>
+                </div>
+                <button onClick className={`${style.log}`}><BsPersonCircle /> Login</button> {/*<BiLogOut /> */}
                 <ReactModal>
                     <Login />
                 </ReactModal>
--- src/films/index.js
+++ src/films/index.js
@@ -15,10 +15,23 @@ function Films(){
 
                 </div>
             </div>
-            <div className={`${style.display}`}>{/*dispaly */}
+            <div className={`${style.display}`}>{/*display */}
                 <div>{/*film display */}
                     <div className={`${style.container1}`}>{/*container */}
+                        <div className={`${style.contain}`}>{/*img */}
+                            <img className={`${style.img}`} src="https://www.greenlawnfertilizing.com/hubfs/Imported_Blog_Media/lawn-540x540_jpg-Dec-06-2023-05-34-10-0199-PM.webp"></img>
+                        </div>
+                        <div className={`${style.content}`}>{/*rest */}
+                            <div className={`${style.title}`}>{/*title */}
+                                <h1 className={`${style.h1}`}>Zale</h1>
+                            </div>
+                            <div>{/*airing info */}
 
+                            </div>
+                            <div>{/*rest */}
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
